347115baebb2617997da7a96fc9655d547d9faf5
Miguel Palomera <miguel.palomera@gmail.com>
2020-10-14 00:18:49 +0000

    p6

--- practicas/4/creaprocesos.c
+++ practicas/4/creaprocesos.c
@@ -10,14 +10,20 @@ void signalHandler(int signum) {
   printf("Mi hijo termino con status %d",status);
 }
 
+void controlC(int singnum){
+
+}
+
+
 int main(int argc, char **argv) {
   signal(17, signalHandler);
+  signal(2,controlC);
   unsigned pid = fork();
   if (pid == 0) {
       sleep(5);
     printf("Soy el proceso hijo\n");
     // execv(argv[1],&argv[1]);
-    return 10;
+    return 1;
   } else {
     printf("Soy el proceso padre y mi hijo es %u\n", pid);
     while (1) {
